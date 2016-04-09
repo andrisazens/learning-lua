19d31b089ebc60caba8245296e09c89920b6896a
Andris Azens <andris.azens@inbox.lv>
2016-04-09 07:56:52 +0300

    first n primes ending with 3

--- first.lua
+++ first.lua
@@ -36,18 +36,39 @@ function format_is_prime(num)
 end
 
 function first_primes(n)
+    local i, found_cnt, result = 1, 0, {}
+    while found_cnt < n do        
+        if (is_prime(i) and ends_in_3(i)) then            
+            found_cnt = found_cnt + 1
+            result[found_cnt] = i
+            
+            if (found_cnt >= n) then
+                break
+            end
+        end
+        
+        i = i + 1
+    end
     
+    local formatted = "Found " .. tostring(found_cnt) .. " prime numbers:"
+    for i = 1, found_cnt do
+        formatted = formatted .. " " .. result[i]
+    end
+    
+    return formatted
 end
 
 --print(ends_in_3(3))
 --print(ends_in_3(4))
 --print(ends_in_3(53))
 
-print(format_is_prime(1))
-print(format_is_prime(2))
-print(format_is_prime(3))
-print(format_is_prime(4))
-print(format_is_prime(5))
-print(format_is_prime(10))
-print(format_is_prime(23))
-print(format_is_prime(25))
+-- print(format_is_prime(1))
+-- print(format_is_prime(2))
+-- print(format_is_prime(3))
+-- print(format_is_prime(4))
+-- print(format_is_prime(5))
+-- print(format_is_prime(10))
+-- print(format_is_prime(23))
+-- print(format_is_prime(25))
+
+print(first_primes(10))
