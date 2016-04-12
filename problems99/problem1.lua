c80a174dd10f6f1a70f9e72edf7116fed966f964
Andris Azens <andris.azens@inbox.lv>
2016-04-12 19:49:00 +0300

    Second problem

--- problems99/problem1.lua
+++ problems99/problem1.lua
@@ -1,7 +1,36 @@
---P01 (*) Find the last box of a list. Example: (my-last '(a b c d))
+--P01 (*) Find the last box of a list. Example: (my-last '(a b c d)) => d
 
 function p01(list)
     return list[table.getn(list)]    
 end
 
+-- P02 (*) Find the last but one box of a list. Example: (my-but-last '(a b c d)) => (C D)
+
+function p02(list)
+    if table.getn(list) > 2 then
+        return { list[#list -1 ], list[#list] }
+    else
+        return list
+    end
+end
+
+function table_to_string(t)
+    local result = {}
+    for k, v in pairs(t) do
+        result[#result + 1] = v
+    end
+    
+    return table.concat(result, " ")
+end
+
+mt = {
+    __tostring = table_to_string
+}
+
+local p02result = p02({'a', 'b', 'c', 'd'}) 
+setmetatable(p02result, mt)
+
 print(p01({'a', 'b', 'c', 'd'}))
+
+print(p02result)
+
