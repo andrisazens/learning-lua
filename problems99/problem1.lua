80c7729e6f5a75fd2c5224343b59b5cd35f7db95
Andris Azens <andris.azens@inbox.lv>
2016-04-14 21:51:57 +0300

    P04 and P05

--- problems99/problem1.lua
+++ problems99/problem1.lua
@@ -19,6 +19,24 @@ function p03(list, num)
     return list[num];
 end
 
+-- P04 (*) Find the number of elements of a list. 
+function  p04(list)
+    return #list
+end
+
+-- P05 (*) Reverse a list.
+function p05(list)
+    local result = {}
+        
+    local i = 1
+    while #result < #list do
+        result[i] = list[#list - i + 1]
+        i = i + 1
+    end
+    
+    return result
+end
+
 function table_to_string(t)
     local result = {}
     for k, v in pairs(t) do
@@ -35,9 +53,13 @@ mt = {
 local p02result = p02({'a', 'b', 'c', 'd'}) 
 setmetatable(p02result, mt)
 
-print(p01({'a', 'b', 'c', 'd'}))
+local p05result = p05({'a', 'b', 'c', 'd', 'e'})
+setmetatable(p05result, mt)
 
-print(p02result)
+--print(p01({'a', 'b', 'c', 'd'}))
+--print(p02result)
+--print(p03({'a', 'b', 'c', 'd', 'e'}, 3))
+--print(p04({'a', 'b', 'c', 'd'}))
 
-print(p03({'a', 'b', 'c', 'd', 'e'}, 3))
+print(p05result)
 
