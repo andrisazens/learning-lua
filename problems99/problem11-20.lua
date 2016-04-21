d3470fd796877889c9f12a5d55343e6a516301a9
Andris Azens <andris.azens@inbox.lv>
2016-04-21 21:49:16 +0300

    P12 solved

--- problems99/problem11-20.lua
+++ problems99/problem11-20.lua
@@ -1,3 +1,24 @@
+-- P12 (**) Decode a run-length encoded list. Given a run-length code list generated as specified in problem P11. Construct its uncompressed version.
+function p12(list)
+    local result = {}
+    
+    for i = 1, #list do
+        local subListLen = #list[i]
+        if subListLen == 1 then
+            result[#result + 1] = list[i][1]
+        else
+            local charCount = list[i][1]
+            local char = list[i][2]
+            
+            for j = 1, charCount do
+                result[#result + 1] = char
+            end
+        end
+    end
+        
+    return result
+end
+
 -- P11 (*) Modified run-length encoding. Modify the result of problem P10 in such a way that if an element has no duplicates 
 --   it is simply copied into the result list. Only elements with duplicates are transferred as (N E) lists.
 -- Example: * (encode-modified '(a a a a b c c a a d e e e e)) => ((4 A) B (2 C) (2 A) D (4 E))
@@ -61,4 +82,8 @@ mt = {
 local p11result = p11({'a', 'a', 'a', 'b', 'c', 'c' })
 setmetatable(p11result, mt)
 
+local p12result = p12({{3, 'a'}, {'b'}, {2,'c'}})
+setmetatable(p12result, mt)
+
 print(p11result)
+print(p12result)
