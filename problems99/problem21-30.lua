b2a031a39e5085f6a290361660549a90ebe8a7d5
Andris Azens <andris.azens@inbox.lv>
2016-05-04 17:12:21 +0300

    P26 problem

--- problems99/problem21-30.lua
+++ problems99/problem21-30.lua
@@ -1,3 +1,17 @@
+--P26 (**) Generate the combinations of K distinct objects chosen from the N elements of a list
+--In how many ways can a committee of 3 be chosen from a group of 12 people? We all know that there are C(12,3) = 220 possibilities (C(N,K) 
+-- denotes the well-known binomial coefficients). For pure mathematicians, this result may be great. But we want to really generate all the possibilities in a list.
+-- Example: * (combination 3 '(a b c d e f)) => ((A B C) (A B D) (A B E) ... )
+function p26(combinationCnt, list)
+    local result, iter = {}, combinationCnt
+    
+    -- while iter < #list do
+    --     for i = 
+    -- end
+    
+    return result
+end
+
 -- P25 (*) Generate a random permutation of the elements of a list.
 -- Example: * (rnd-permu '(a b c d e f)) => (B A D C E F)
 function p25(list)    
